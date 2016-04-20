8f5b4a7ddc2ff9f022c3d45ff91a82174d682c90
Voidling <eli.stonium@gmail.com>
2016-04-20 14:47:03 -0600

    Pix almost works :O

--- src/Pix.java
+++ src/Pix.java
@@ -1,24 +1,52 @@
+import java.util.Random;
+
 /**
  * Created by kogmaw on 4/20/16.
  */
 public class Pix {
-    String parsedString;
+    String[] parsedStrings;
+    String outputString;
     public Pix(String unparsedString){
-        parsedString = parseString(unparsedString);
-
-
-
-
-
+        parsedStrings = parseString(unparsedString);
+        generateChain();
 
     }
 
 
-    private String parseString(String unparsedString){
+    private String[] parseString(String unparsedString){
+
+        if (!unparsedString.endsWith(".")){
+            unparsedString += ".";//End it with a period
+        }
+        while (unparsedString.contains("\n")){
+            unparsedString.replace("\n", " ");//Replace returns with space
+        }
+        while (unparsedString.contains("  ")){
+            unparsedString.replace("  ", " "); //2 becomes 1
+        }
+
+        return unparsedString.split(" ");
+    }
+
+    private void generateChain(){
+        Random stringPicker = new Random(System.nanoTime());
+        int stringPicked = (int) Math.floor(parsedStrings.length * stringPicker.nextDouble());
+
+        for (int i = 0; i < 5000; i++){
+            outputString += parsedStrings[stringPicked];
+
+            if (stringPicker.nextDouble() > 0.8){
+                //TODO custom randomness
+                stringPicked = (int) Math.floor(parsedStrings.length * stringPicker.nextDouble());
+
+
+            }
+
+
+        }
 
 
 
-        return unparsedString;
     }
 
 
