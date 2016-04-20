ab6e46138735e47c32bbe7a4045af8efa9b02c05
Voidling <eli.stonium@gmail.com>
2016-04-20 14:29:38 -0600

    Added Lulu and Pix

---
+++ src/Lulu.java
@@ -0,0 +1,19 @@
+/**
+ * Created by kogmaw on 4/20/16.
+ */
+public class Lulu {
+    String userString;
+
+    public Lulu(){
+        userString = "The Awakening of kings was an event marked by the ultimate battle of the century. It lead to the" +
+                "\nultimate battle between good and evil. If there were ever an event which needed more popularity, it would" +
+                "\n      be this one. This string is to simulate different kinds of grammar and also the parsing that would be" +
+                "\nrequired to get this thing working. slash n should become space and multiple      spaces in a row should be removed" +
+                "\n Using Regex makes this whole thing possible if I do it right. I hope   "
+
+
+    }
+
+
+
+}
---
+++ src/Pix.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kogmaw on 4/20/16.
+ */
+public class Pix {
+}
